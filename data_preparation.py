aa3b8e90492f409bd3028143d63a70e48dd00d22
rafalsosnowski <rafalsosnowski4@wp.pl>
2024-04-29 20:47:48 +0200

    todo

--- data_preparation.py
+++ data_preparation.py
@@ -11,22 +11,11 @@ import scipy.io.wavfile as wav
 
 
 def make_spec(file, file_dir, flip=False, ps=False, st = 4):
-    """
-    create a melspectrogram from the amplitude of the sound
-    
-    Args:
-        file (str): filename
-        file_dir (str): directory path
-        flip (bool): reverse time axis
-        ps (bool): pitch shift
-        st (int): half-note steps for pitch shift
-    Returns:
-        np.array with shape (122,85) (time, freq)
-    """
+
     
     sig, sr = librosa.load(file_dir+'/audio/'+file, sr=16000)
     
-    if len(sig) < 16000: #pad shorter than 1 sec audio with ramp to zero
+    if len(sig) < 16000: 
         sig = np.pad(sig, (0,16000-len(sig)), "linear_ramp")
         
     if ps:
@@ -47,19 +36,11 @@ def make_spec(file, file_dir, flip=False, ps=False, st = 4):
 
 
 def split_arr(arr):
-    """
-    split an array into chunks of length 16000
-    Returns:
-        list of arrays
-    """
+
     return np.split(arr, np.arange(16000, len(arr), 16000))
 
 
 def create_silence(train_dir):
-    """
-    reads wav files in background noises folder, 
-    splits them and saves to silence folder in train_dir
-    """
     for file in os.listdir(os.path.join(train_dir,"_background_noise_/")):
         if ".wav" in file:
             sig, sr = librosa.load(os.path.join(train_dir,"_background_noise_/")+file, sr = 16000) 
@@ -67,11 +48,11 @@ def create_silence(train_dir):
             if not os.path.exists(train_dir+"/audio/silence/"):
                 os.makedirs(train_dir+"/audio/silence/")
             for ind, arr in enumerate(sig_arr):
-                file_name = "frag%d" %ind + "_%s" %file # example: frag0_running_tap.wav
+                file_name = "frag%d" %ind + "_%s" %file
                 sf.write(train_dir+"/audio/silence/"+file_name, arr, 16000)
                 
 def get_validation_list(train_dir):
-    with open(train_dir+"/testing_list.txt") as val_list:
+    with open(train_dir+"/validation_list.txt") as val_list:
         validation_list = [row[0] for row in csv.reader(val_list)]
     assert len(validation_list) == 6835, "testing files not loaded"
     for i, file in enumerate(os.listdir(train_dir+"audio/silence/")):
@@ -93,7 +74,6 @@ def get_test_val_lists(train_dir, validation_list):
                 training_list.append(folder+'/'+f)
             class_counts[folder] = i
 
-    #remove filenames from validation_list that don't exist anymore (due to eda)
     validation_list = list(set(validation_list).intersection(all_files_list))
     return validation_list, training_list
 def get_all_classes(train_dir):
@@ -105,7 +85,6 @@ def get_all_classes(train_dir):
         
     folders = os.listdir(train_dir+"/audio")
     
-    # put folders in same order as in the classes list, used when making sets
     all_classes = [x for x in classes]
     for ind, cl in enumerate(folders):
         if cl not in classes:
@@ -144,11 +123,11 @@ def compute_fbank(file):
                                      zero_padding=True)
 
 
-    power_spectrum = processing.power_spectrum(frames, fft_points=512) # num_frames x fft_length
+    power_spectrum = processing.power_spectrum(frames, fft_points=512)
     
     log_fbank = feature.lmfe(signal_preemphasized,sampling_frequency=sr,frame_length=0.025,
                         frame_stride=0.010,num_filters=80,
-                         fft_length=512, low_frequency=0,high_frequency=None) # num_frames x num_filters
+                         fft_length=512, low_frequency=0,high_frequency=None) 
 
 
     if apply_cmvn:
@@ -160,8 +139,8 @@ def compute_fbank(file):
 
 def wav_padding(wav_data, wav_max_len, feature_dim):    
     new_wav_data_lst = np.zeros(
-        (wav_max_len, feature_dim),dtype=np.float32) # 如果增加了一阶和二阶导数则是三个channel，分别是static, first and second derivative features
+        (wav_max_len, feature_dim),dtype=np.float32)
     
     new_wav_data_lst[:wav_data.shape[0], :] = wav_data
-    # print('new_wav_data_lst',new_wav_data_lst.shape,wav_lens.shape)
+
     return new_wav_data_lst
