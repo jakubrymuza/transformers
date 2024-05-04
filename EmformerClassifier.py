964a7f6fefb65fd2480d03e20a0eeee537eeead7
jakubrymuza <jakubrymuza@gmail.com>
2024-05-04 21:03:46 +0200

    add enformer etc

--- EmformerClassifier.py
+++ EmformerClassifier.py
@@ -2,19 +2,19 @@ import torch
 import numpy as np
 import torch.nn as nn
 
-from torchaudio.models import Conformer
+from torchaudio.models import Emformer
 
-class ConformerClassifier(nn.Module):
-    def __init__(self, num_classes, input_length = 122, input_dim = 85, num_heads = 5, ffn_dim = 512, num_layers = 8, dropout = 0, depthwise_conv_kernel_size = 31):
-        super(ConformerClassifier, self).__init__()
+class EmformerClassifier(nn.Module):
+    def __init__(self, num_classes, input_length = 122, input_dim = 85, num_heads = 5, ffn_dim = 512, num_layers = 8, segment_length = 4, dropout = 0):
+        super(EmformerClassifier, self).__init__()
         self.input_length = input_length
 
-        self.encoder = Conformer(
+        self.encoder = Emformer(
             input_dim = input_dim,
             num_heads = num_heads,
             ffn_dim = ffn_dim,
             num_layers = num_layers,
-            depthwise_conv_kernel_size = depthwise_conv_kernel_size,
+            segment_length = segment_length,
             dropout = dropout,
         )
         self.fc = nn.Linear(input_dim, num_classes)
--- experiments.py
+++ experiments.py
@@ -1,5 +1,4 @@
 import numpy as np
-from PIL import Image
 import matplotlib.pyplot as plt
 import random
 
@@ -7,7 +6,6 @@ from sklearn.metrics import classification_report, confusion_matrix, ConfusionMa
 
 import torch
 from torch.utils.data import DataLoader
-from augmentation import mixup_data, mixup_criterion
 
 RANDOM_SEED = 17
 NUM_WORKERS = 0
@@ -19,7 +17,7 @@ CLASSES = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go'
 np.random.seed(RANDOM_SEED)
 torch.manual_seed(RANDOM_SEED)
 
-def perfrom_test(model, train_data, test_data, num_epochs, criterion, optimizer, batch_size, all_classes, device, scheduler = None, should_print = True, random_seed=RANDOM_SEED):
+def perform_test(model, train_data, test_data, num_epochs, criterion, optimizer, batch_size, all_classes, device, scheduler = None, should_print = True, random_seed=RANDOM_SEED, trans_mode = False):
     generator = torch.Generator().manual_seed(random_seed)
     random.seed(random_seed)
     torch.manual_seed(random_seed)
@@ -35,22 +33,23 @@ def perfrom_test(model, train_data, test_data, num_epochs, criterion, optimizer,
                                         all_classes = all_classes,
                                         device = device,
                                         valid_data = test_data,
-                                        scheduler = scheduler)
+                                        scheduler = scheduler,
+                                        trans_mode = trans_mode)
     
     # evaluating model
     if should_print:
         print_err(total_loss_all)
         
         print("Train eval:")
-        evaluate(model = model, dataset = train_data, device = device, all_classes = all_classes)
+        evaluate(model = model, dataset = train_data, device = device, all_classes = all_classes, trans_mode = trans_mode)
         
         print("Test eval:")
-        evaluate(model = model, dataset = test_data, device = device, all_classes = all_classes)
+        evaluate(model = model, dataset = test_data, device = device, all_classes = all_classes, trans_mode = trans_mode)
         
         
     return model   
 
-def run_network(model, criterion, optimizer, dataloader, num_epochs, device, scheduler, valid_data, all_classes, should_print = True, test_frequency = 10):
+def run_network(model, criterion, optimizer, dataloader, num_epochs, device, scheduler, valid_data, all_classes, trans_mode, should_print = True, test_frequency = 1):
     total_loss = []
     model.train()
 
@@ -59,6 +58,9 @@ def run_network(model, criterion, optimizer, dataloader, num_epochs, device, sch
         loss = 0
     
         for images, labels in dataloader:
+            if(trans_mode):
+                inputs = inputs.permute(0, 2, 1)
+
             if device == torch.device('cuda'):
                 images = images.cuda()
                 labels = labels.cuda()
@@ -66,6 +68,10 @@ def run_network(model, criterion, optimizer, dataloader, num_epochs, device, sch
             optimizer.zero_grad()
             
             outputs = model(images)
+
+            if(trans_mode):
+                labels = labels.long()
+
             loss = criterion(outputs, labels)
 
             loss.backward()
@@ -77,11 +83,10 @@ def run_network(model, criterion, optimizer, dataloader, num_epochs, device, sch
         
         total_loss.append(loss)
         if should_print:
-            lr = optimizer.param_groups[0]["lr"]
-            print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss / len(dataloader):.4f}, Lr: {lr}")
+            print(f"Epoch [{epoch+1}/{num_epochs}], Train loss: {loss / len(dataloader):.4f}")
 
             if (epoch + 1) % test_frequency == 0 and (epoch + 1) < num_epochs:
-                evaluate(model = model, dataset = valid_data, device = device, long_mode = False, all_classes = all_classes)
+                evaluate(model = model, dataset = valid_data, device = device, long_mode = False, all_classes = all_classes, trans_mode = trans_mode)
         
     return model, total_loss
 
@@ -98,48 +103,55 @@ def print_err(total_loss):
     plt.ylabel('error')
     plt.title('error in each epoch')
     
-def evaluate(model, dataset, device, all_classes, long_mode = True):
+def evaluate(model, dataset, device, all_classes, trans_mode, long_mode = True):
     dataloader = DataLoader(dataset, batch_size = 100, num_workers = NUM_WORKERS)
     model.eval()
     y_true = []
     y_pred = []
     with torch.no_grad():
         for images, labels in dataloader:
+            if(trans_mode):
+                inputs = inputs.permute(0, 2, 1)
+
             if device == torch.device('cuda'):
                 images = images.cuda()
                 labels = labels.cuda()
 
             outputs = model(images)
-            
+
             images = images.cpu()
             labels = labels.cpu()
             outputs = outputs.cpu()
+
+            if(trans_mode):
+                _, outputs = torch.max(torch.tensor(outputs), 1)
 
             _, predicted = torch.max(outputs, 1)
             y_true.extend(labels.numpy())
             y_pred.extend(predicted.numpy())
 
     # converting classes
-    class_dict = dict(all_classes)
-    y_true = np.array([class_dict[num] if class_dict.get(num) in VAL_CLASSES else "unknown" for num in y_true])
-    y_pred = np.array([class_dict[num] if class_dict.get(num) in VAL_CLASSES else "unknown" for num in y_pred])
+    # class_dict = dict(all_classes)
+    # y_true = np.array([class_dict[num] if class_dict.get(num) in VAL_CLASSES else "unknown" for num in y_true])
+    # y_pred = np.array([class_dict[num] if class_dict.get(num) in VAL_CLASSES else "unknown" for num in y_pred])
 
-    class_to_number = {class_name: i for i, class_name in enumerate(CLASSES)}
+    # class_to_number = {class_name: i for i, class_name in enumerate(CLASSES)}
 
-    y_true = np.array([class_to_number[class_dict[num]] if class_dict.get(num) in CLASSES else -1 for num in y_true])
-    y_pred = np.array([class_to_number[class_dict[num]] if class_dict.get(num) in CLASSES else -1 for num in y_pred])
+    # y_true = np.array([class_to_number[class_dict[num]] if class_dict.get(num) in CLASSES else -1 for num in y_true])
+    # y_pred = np.array([class_to_number[class_dict[num]] if class_dict.get(num) in CLASSES else -1 for num in y_pred])
 
-    target_names = [CLASSES[cls] for cls in range(12)]
+    #target_names = [CLASSES[cls] for cls in range(12)]
 
     if long_mode:
         print(classification_report(y_true = y_true, 
                                     y_pred = y_pred, 
-                                    target_names = target_names,
+                                    #target_names = target_names,
                                     digits=4))
 
         cm = confusion_matrix(y_true = y_true, y_pred = y_pred)
             
-        disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = target_names)
+        disp = ConfusionMatrixDisplay(confusion_matrix = cm, #display_labels = target_names
+                                      )
         disp.plot()
     else:
         acc = accuracy_score(y_true = y_true, y_pred = y_pred)
